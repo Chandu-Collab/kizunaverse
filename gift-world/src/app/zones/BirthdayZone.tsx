39c171909664df2d64f83aff3291e52e275be202
Chandu-Collab <chandrahasareddy65@gmail.com>
2026-02-18 00:21:20 +0530

    Hospital things need to be fixed

--- gift-world/src/app/zones/BirthdayZone.tsx
+++ gift-world/src/app/zones/BirthdayZone.tsx
@@ -1,9 +1,17 @@
 
 // BirthdayZone cleaned up for new implementation
+
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+const Hospital3D = dynamic(() => import("@/components/3d/Hospital3D"), { ssr: false });
+import Scene from "@/components/3d/Scene";
+
 export default function BirthdayZone() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <span className="text-2xl text-gray-400">Birthday Zone coming soon...</span>
+    <div className="w-full h-screen bg-gradient-to-br from-blue-100 to-green-100 flex items-center justify-center">
+      <Scene cameraPosition={[0, 12, 32]} enableControls enableShadows>
+        <Hospital3D position={[0, 0, 0]} />
+      </Scene>
     </div>
   );
 }
